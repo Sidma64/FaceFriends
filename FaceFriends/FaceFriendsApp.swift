7ffd86af087b6b4a5b582cfb40cdc3762702a780
Sidma64 <sidma.ultra@gmail.com>
2022-01-31 22:15:52 +0300

    Initial Commit

---
+++ FaceFriends/FaceFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFriendsApp.swift
+//  FaceFriends
+//
+//  Created by Uzay Toprak on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
